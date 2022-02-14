69a15b4f6f9cf90831f93338a9bebe98c2cc0f66
whkelvin <whkelvin.hsu@gmail.com>
2022-02-14 19:39:11 -0400

    updated configs

--- powerline-shell/.config/powerline-shell/theme/nord.py
+++ powerline-shell/.config/powerline-shell/theme/nord.py
@@ -1,14 +1,24 @@
 from powerline_shell.themes.default import DefaultColor
 
+nord = 0
+red = 1
+green = 2
+yellow = 3
+blue = 4
+purple = 5
+light_blue = 6
+snow = 7
+nord_light = 8
+teal = 14
 
 class Color(DefaultColor):
     """Basic theme which only uses colors in 0-15 range"""
-    USERNAME_FG = 4
-    USERNAME_BG = 0
-    USERNAME_ROOT_BG = 1
+    USERNAME_FG = blue
+    USERNAME_BG = nord
+    USERNAME_ROOT_BG = red
 
-    HOSTNAME_FG = 7
-    HOSTNAME_BG = 8
+    HOSTNAME_FG = snow
+    HOSTNAME_BG = nord_light
 
     HOME_SPECIAL_DISPLAY = False
     PATH_FG = 0
@@ -19,10 +29,13 @@ class Color(DefaultColor):
     READONLY_BG = 1
     READONLY_FG = 15
 
-    REPO_CLEAN_BG = 2   # green
-    REPO_CLEAN_FG = 0   # black
-    REPO_DIRTY_BG = 1   # red
-    REPO_DIRTY_FG = 15  # white
+    REPO_CLEAN_BG = nord 
+    REPO_CLEAN_FG = green
+    REPO_DIRTY_BG = nord
+    REPO_DIRTY_FG = yellow
+
+    SVN_CHANGES_BG = 7
+    SVN_CHANGES_FG = 4  # dark green
 
     JOBS_FG = 14
     JOBS_BG = 8
@@ -43,3 +56,26 @@ class Color(DefaultColor):
 
     TIME_FG = 8
     TIME_BG = 7
+
+    RESET = -1
+
+    GIT_AHEAD_BG = nord
+    GIT_AHEAD_FG = purple
+
+    GIT_BEHIND_BG = nord
+    GIT_BEHIND_FG = purple
+
+    GIT_STAGED_BG = nord
+    GIT_STAGED_FG = purple
+
+    GIT_NOTSTAGED_BG = nord
+    GIT_NOTSTAGED_FG = purple
+
+    GIT_UNTRACKED_BG = nord
+    GIT_UNTRACKED_FG = purple
+
+    GIT_CONFLICTED_BG = nord
+    GIT_CONFLICTED_FG = purple
+
+    GIT_STASH_BG = nord
+    GIT_STASH_FG = purple
