91b58d151d1a9b85ff6601a3884d96f4ec5e8272
whkelvin <whkelvin.hsu@gmail.com>
2022-04-15 17:13:51 -0300

    fixed fish keybinding in non vi mode again

--- fish/.config/fish/functions/configure_goldfish.fish
+++ fish/.config/fish/functions/configure_goldfish.fish
@@ -4,7 +4,7 @@ function configure_goldfish
 
   alias goldfish='cat ~/.config/goldfish/goldfish.config | fzf --preview-window="bottom:3:wrap" --preview="echo {} | awk -F \' <>< \' \'{print \$2}\' | fish_indent --ansi" | awk -F \' <>< \' \'{print $2}\''
 
-  if test "$fzf_key_bindings" = "fish_vi_key_bindings"
+  if test "$fish_key_bindings" = "fish_vi_key_bindings"
     bind -M insert \eg fzf_search_goldfish
   else
     bind \eg fzf_search_goldfish
