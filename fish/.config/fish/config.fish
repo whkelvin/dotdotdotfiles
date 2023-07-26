e52cc8e5429463c07ab625daaeca5a6f7d773346
whkelvin <whkelvin.hsu@gmail.com>
2023-07-25 23:55:00 -0300

    updated dot files

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -10,10 +10,82 @@ fzf_configure_bindings --directory=\ec --history=\er --git_log= --git_status= --
 # vim keybinding for fish
 fish_vi_key_bindings
 
+function bg_fg 
+  test $argv[1] != "" && set_color -b $argv[1]
+  test $argv[1] != "" && set_color $argv[2]
+end
+
+function fish_mode_prompt
+  set b '2E3440'
+  set w 'D8DEE9'
+  set t black
+
+  switch $fish_bind_mode
+    case default
+      bg_fg $t $b
+      echo -n ""
+      bg_fg $b yellow
+      echo ' [N] '
+    case insert
+      bg_fg $t $b
+      echo -n ""
+      bg_fg $b green
+      echo ' [I] '
+    case replace_one
+      bg_fg $t $b
+      echo -n ""
+      bg_fg $b magenta
+      echo ' [R] '
+    case visual
+      bg_fg $t $b
+      echo -n ""
+      bg_fg $b blue
+      echo ' [V] '
+    case '*'
+      set_color --bold red
+      echo '?'
+  end
+  bg_fg black white
+end
+
 function fish_prompt
-    powerline-shell --shell bare $status
+  set b '2E3440'
+  set w 'D8DEE9'
+  set t black
+
+  bg_fg $b $w
+  echo -n $USER
+  bg_fg $b $b
+  echo -n "█"
+  bg_fg $w $b
+  echo -n ""
+  bg_fg $b $w
+  echo -n ""
+  bg_fg $w $b
+  echo -n ""
+  bg_fg $w $b
+  echo -n " "
+  echo -n $(prompt_pwd)
+  echo -n " "
+  bg_fg $t $w
+  echo -n ""
+
+  set branch $(fish_git_prompt)
+  if test -n "$branch"
+    bg_fg $b blue
+    echo -n "   $(git rev-parse --abbrev-ref HEAD) "
+    bg_fg $t $b
+    echo -n ""
+  end
+
+
+
+  bg_fg black white
+  echo " "
 end
 
 use_terminal_colors
 configure_goldfish
+
+
 
